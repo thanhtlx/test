3dd33038db20191bfca59ae1ea43b4202a852003
thanhtlx <thanh1908.xx@gmail.com>
2022-11-09 15:31:59 +0700

    first commit

---
+++ test.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+ 
+void add(int a, int b)
+{
+  cout << "sum = " << (a + b);
+}
+ 
+void add(int a, int b, int c)
+{
+    cout << endl << "sum = " << (a + b + c);
+}
+ 
+// Driver code
+int main()
+{
+    add(10, 2);
+    add(5, 6, 4);
+ 
+    return 0;
+}
