a75dacac1d5a9fcc51ddf8fda29927d346db7819
thanhtlx <thanh1908.xx@gmail.com>
2024-03-18 07:15:36 +0700

    add feature 123

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello INT2204 1!");
+        System.out.println("Hello INT2204 1!");
     }
 }
