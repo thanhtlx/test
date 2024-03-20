07ae629332bb09528ce191399c767bd047af2028
thanhtlx <thanh1908.xx@gmail.com>
2024-03-20 13:17:56 +0700

    update test.java

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("123");
         System.out.println("Hello World! xyz");
         System.out.println("Hello INT2204 1!");
         System.out.println("Hello INT2204 1!");
